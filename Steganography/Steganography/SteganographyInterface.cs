9a05e7d195c9a222cb5a61ad9b17759dc228f7f3
papyruscompendium <papyruscompendium@protonmail.com>
2020-08-28 13:30:25 -0400

    Styling Edits
    Edited the styling to the code to match my newer more concise code styling.

--- Steganography/Steganography/SteganographyInterface.cs
+++ Steganography/Steganography/SteganographyInterface.cs
@@ -16,34 +16,33 @@ namespace Steganography
     {
         private SteganographicImage Stegnograph { get; set; }
 
-        public SteganographyInterface()
-        {
+        public SteganographyInterface() =>
             InitializeComponent();
-        }
 
-        public void CalculateImageCapacity() => imageCapacity.Text = $"Max Capacity: {Stegnograph.TotalCapacity()} bytes";
-        private void SecretMessageText_TextChanged(object sender, EventArgs e) => secretMessageSize.Text = $"Data Size: {Encoding.UTF8.GetBytes(secretMessageText.Text).Length} bytes";
-        private void SteganographyInterface_Shown(object sender, EventArgs e) => secretMessageSize.Text = $"Data Size: {Encoding.UTF8.GetBytes(secretMessageText.Text).Length} bytes";
-        private void GenerateKeyButton_Click(object sender, EventArgs e) => aesPassword.Text = Cryptography.GenerateSecurePassword(10);
+        public void CalculateImageCapacity() => imageCapacity.Text = $"Max Capacity: {Stegnograph.TotalCapacity} bytes";
+        private void SecretMessageText_TextChanged(object sender, EventArgs e) =>
+            secretMessageSize.Text = $"Data Size: {Encoding.UTF8.GetBytes(secretMessageText.Text).Length} bytes";
+        private void SteganographyInterface_Shown(object sender, EventArgs e) =>
+            secretMessageSize.Text = $"Data Size: {Encoding.UTF8.GetBytes(secretMessageText.Text).Length} bytes";
+        private void GenerateKeyButton_Click(object sender, EventArgs e) =>
+            aesPassword.Text = Cryptography.GenerateSecurePassword(10);
 
         private void ImageSelectionLink_Click(object sender, EventArgs e)
         {
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-                Stegnograph = new SteganographicImage(new Bitmap(Image.FromFile(openFileDialog.FileName)));
-            else
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
                 return;
 
+            Stegnograph = new SteganographicImage(new Bitmap(Image.FromFile(openFileDialog.FileName)));
             inputImage.Image = Stegnograph.Stegnograph;
             CalculateImageCapacity();
         }
 
         private void SaveImageLink_Click(object sender, EventArgs e)
         {
-            if (Stegnograph == null)
+            if (Stegnograph == null || saveFileDialog.ShowDialog() != DialogResult.OK)
                 return;
 
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                Stegnograph.Stegnograph.Save(saveFileDialog.FileName, ImageFormat.Png);
+            Stegnograph.Stegnograph.Save(saveFileDialog.FileName, ImageFormat.Png);
         }
 
         private void ConvertImageButton_Click(object sender, EventArgs e)
@@ -54,15 +53,14 @@ namespace Steganography
                 return;
             }
 
-            if (encodeRadio.Checked)
-                if (Stegnograph.FitsIntoImage(Encoding.UTF8.GetBytes(secretMessageText.Text)))
-                    Task.Run(() =>
-                    {
-                        outputImage.Image = Stegnograph.EncodeImage(Encoding.UTF8.GetBytes(secretMessageText.Text), aesCheckBox.Checked, aesPassword.Text);
-                        MessageBox.Show("Encoded!");
-                    });
-                else
-                    MessageBox.Show("Data too large!");
+            if (encodeRadio.Checked && Stegnograph.FitsIntoImage(Encoding.UTF8.GetBytes(secretMessageText.Text)))
+                Task.Run(() =>
+                {
+                    outputImage.Image = Stegnograph.EncodeImage(Encoding.UTF8.GetBytes(secretMessageText.Text), aesCheckBox.Checked, aesPassword.Text);
+                    MessageBox.Show("Encoded!");
+                });
+            else
+                MessageBox.Show("Data too large!");
 
             if (decodeRadio.Checked)
                 Task.Run(() =>
